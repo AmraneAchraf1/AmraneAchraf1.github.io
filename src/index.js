cf78ee683f28f45da5852067c106067d3c323f97
AmraneAchraf1 <Amrane.Achraf1@ofppt-edu.ma>
2023-08-20 15:38:48 +0100

    setup mui and joyui

--- src/index.js
+++ src/index.js
@@ -1,25 +1,127 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Main from './layout/Main';
-import About from './pages/About';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// setup mui with joyui
+import {
+  experimental_extendTheme as materialExtendTheme,
+  Experimental_CssVarsProvider as MaterialCssVarsProvider,
+  THEME_ID as MATERIAL_THEME_ID,
+} from "@mui/material/styles";
+import { CssVarsProvider as JoyCssVarsProvider, extendTheme } from "@mui/joy/styles";
+
+// styles
+import "./index.css";
+//pages
+import Main from "./layout/Main";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import { dark, light } from "@mui/material/styles/createPalette";
+
+// Material UI theme configuration 
+const materialTheme = materialExtendTheme(
+  {
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#62B250",
+        
+      },
+      secondary: {
+        main: "#1c1c1c",
+        dark: "#acacac",
+      },
+      
+      background: {
+        default: "#1c1c1c",
+        paper: "#1c1c1c",
+      }
+    },
+  },
+  {
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            borderRadius: "16px",
+            color : "#FFFFFF",
+          },
+        },
+      },
+    },
+
+  },
+);
+
+// JoyUI theme configuration 
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      
+      palette: {
+        primary: {
+          100: "#e0f0dc",
+          200: "#c0e0b9",
+          300: "#a1d196",
+          400: "#81c173",
+          500: "#62b250",
+          600: "#4e8e40",
+          700: "#3b6b30",
+          800: "#274720",
+          900: "#142410"
+      },
+      neutral: {
+        100: "#d2d2d2",
+        200: "#a4a4a4",
+        300: "#777777",
+        400: "#494949",
+        500: "#1c1c1c",
+        600: "#161616",
+        700: "#111111",
+        800: "#0b0b0b",
+        900: "#060606"
+      },
+    },
+  },
+  },
+  
+  components: {
+    JoyButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "16px",
+          color : "#FFFFFF",
+        },
+      },
+    },
+  },
+
+});
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Main />,
     children: [
       {
-        path: '/',
+        path: "/",
+        element: <Home />,
+        index: true,
+      },
+      {
+        path: "/about",
         element: <About />,
-        index: true
-      }
-    ]
-  }
+      },
+    ],
+  },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <RouterProvider router={router} />
+  <MaterialCssVarsProvider defaultMode="dark"  theme={{ [MATERIAL_THEME_ID]: materialTheme }}>
+    <JoyCssVarsProvider defaultMode="dark"  theme={ theme} >
+      <RouterProvider router={router} />
+    </JoyCssVarsProvider>
+  </MaterialCssVarsProvider>
 );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,12 @@
+import { Button, Container } from '@mui/joy'
 import React from 'react'
 
 const About = () => {
   return (
-    <div>Aboute</div>
+    <Container>
+      <h1>About</h1>
+      <Button >Primary</Button>
+    </Container>
   )
 }
 
