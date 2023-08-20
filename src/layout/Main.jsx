da3dabd6e5a665aa3b3b427c6b9cb2df020785a9
AmraneAchraf1 <Amrane.Achraf1@ofppt-edu.ma>
2023-08-20 15:38:48 +0100

    setup mui and joyui

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,17 @@
+import {  Container, Button as JoyBtn } from '@mui/joy'
+import { Button } from '@mui/material'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 const Main = () => {
   return (
+    <Container >
+    <Button variant="outlined"  >4  ago</Button>
+    <JoyBtn color="primary" >
+    JoyBtn
+    </JoyBtn>
     <Outlet />
+    </Container>
   )
 }
 
